c4cff93533da0ad3511818fbe5551603b537ec4b
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-12-22 03:48:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const Api = (() => {
   const url = 'http://localhost:4232/courseList';
-  
 
-  const getCro = () =>
-    fetch(url).then((response) => response.json());
+  const getCro = () => fetch(url).then((response) => response.json());
   return {
     getCro,
   };
 })();
 
-const View=(()=>{
+const View = (() => {
   const domstr = {
-    selectbtn:'#selectbtn',
-    totalcredit:'#totalcredit',
-    select:'.selection-main',
-    selected:'.selected-main'
-
+    selectbtn: '#selectbtn',
+    totalcredit: '#totalcredit',
+    select: '.selection-main',
+    selected: '.selected-main',
   };
   const createCour = (arr) => {
     let tmp = '';
     arr.forEach((cor) => {
-      var corType=cor.required==false?'Elective':'Compulsory';
+      var corType = cor.required == false ? 'Elective' : 'Compulsory';
       tmp += `
       <p class="doselect" id='${cor.courseId}'>${cor.courseName}<br>Course type: ${corType}<br>Course Credict: ${cor.credit}</p>
       `;
@@ -28,42 +25,48 @@ const View=(()=>{
 
     return tmp;
   };
-  const createCre=(cre)=>{
-    let tmp='';
-    tmp+=`Total credit:<span id="credit">${cre}</span>`;
+  const createCre = (cre) => {
+    let tmp = '';
+    tmp += `Total credit:<span id="credit">${cre}</span>`;
     return tmp;
-
-  }
+  };
   const render = (ele, tmp) => {
     ele.innerHTML = tmp;
   };
-  const pagerender=(select_courses,selected_courses,totalcredit)=>{
+  const pagerender = (select_courses, selected_courses, totalcredit) => {
     const selectcourse = document.querySelector(domstr.select);
     const tmp1 = createCour(select_courses);
 
-    const selectedcourse=document.querySelector(domstr.selected);
-    const tmp2=createCour(selected_courses);
+    const selectedcourse = document.querySelector(domstr.selected);
+    const tmp2 = createCour(selected_courses);
 
-    const total_credit=document.querySelector(domstr.totalcredit);
-    const tmp3=createCre(totalcredit);
+    const total_credit = document.querySelector(domstr.totalcredit);
+    const tmp3 = createCre(totalcredit);
 
     render(selectcourse, tmp1);
-    render(selectedcourse,tmp2);
-    render(total_credit,tmp3);
-    
-
+    render(selectedcourse, tmp2);
+    render(total_credit, tmp3);
+  };
+  const pagecrerender = (totalcredit) => {
+    const total_credit = document.querySelector(domstr.totalcredit);
+    const tmp3 = createCre(totalcredit);
+    render(total_credit, tmp3);
   };
   return {
-    domstr,createCour,createCre,render,pagerender
-  }
+    domstr,
+    createCour,
+    createCre,
+    render,
+    pagerender,
+    pagecrerender,
+  };
 })();
-const Model=((api)=>{
+const Model = ((api) => {
   const { getCro } = api;
   class State {
-    #courses=[];
+    #courses = [];
     #selectedCour = [];
-    #credit=0;
-    
+    #credit = 0;
 
     get courses() {
       return this.#courses;
@@ -73,85 +76,88 @@ const Model=((api)=>{
       this.#courses = newcourse;
     }
 
-    get selectedCour(){
+    get selectedCour() {
       return this.#selectedCour;
     }
-    set selectedCour(Cour){
-      this.#selectedCour=Cour;
+    set selectedCour(Cour) {
+      this.#selectedCour = Cour;
     }
-    get credit(){
+    get credit() {
       return this.#credit;
     }
-    set credit(num){
-      this.#credit=num;
-
+    set credit(num) {
+      this.#credit = num;
     }
   }
-  return {getCro,State}
+  return { getCro, State };
 })(Api);
-const Controller=((model,view)=>{
+const Controller = ((model, view) => {
   const state = new model.State();
-  const init = () =>{
-    model.getCro().then((cor)=>{
-      state.courses=cor;
-      state.selectedCour=[];
-      view.pagerender(state.courses,state.selectedCour,state.credit);
-    })};
+  const init = () => {
+    model.getCro().then((cor) => {
+      state.courses = cor;
+      state.selectedCour = [];
+      view.pagerender(state.courses, state.selectedCour, state.credit);
+    });
+  };
 
-  const select=(()=>{
-    var selectul=document.querySelector(view.domstr.select);
-    window.addEventListener('click',(event)=>{
-      if(event.target.className=='doselect'){
-        
-        event.target.classList.add("style");
-        
-        for(let i=0;i<state.courses.length;i++){
-          if (event.target.id==state.courses[i].courseId){
-           state.credit+=state.courses[i].credit;
-           state.selectedCour.push(state.courses[i]);
-           
+  const select = () => {
+    //var selectul=document.querySelector(view.domstr.select);
+    window.addEventListener('click', (event) => {
+      if (event.target.className == 'doselect') {
+        event.target.classList.add('style');
+
+        for (let i = 0; i < state.courses.length; i++) {
+          if (event.target.id == state.courses[i].courseId) {
+            state.credit += state.courses[i].credit;
+            state.selectedCour.push(state.courses[i]);
           }
         }
         //console.log(state.selectedCour);
-        state.courses=state.courses.filter(item=>item.courseId!=event.target.id);
+        state.courses = state.courses.filter(
+          (item) => item.courseId != event.target.id
+        );
+        view.pagecrerender(state.credit);
         //console.log(state.courses);
-      }
-      else if(event.target.className=='doselect style'){
-        event.target.classList.remove("style");
-        
-        for(let i=0;i<state.selectedCour.length;i++){
-          if (event.target.id==state.selectedCour[i].courseId){
-           state.credit-=state.selectedCour[i].credit;
-           state.courses.push(state.selectedCour[i]);
-           
+      } else if (event.target.className == 'doselect style') {
+        event.target.classList.remove('style');
+
+        for (let i = 0; i < state.selectedCour.length; i++) {
+          if (event.target.id == state.selectedCour[i].courseId) {
+            state.credit -= state.selectedCour[i].credit;
+            state.courses.push(state.selectedCour[i]);
           }
         }
-        state.selectedCour=state.selectedCour.filter(item=>item.courseId!=event.target.id);}
-        //console.log(state.courses);
-    });
-    var getbtn=document.querySelector(view.domstr.selectbtn);
-    getbtn.addEventListener('click',()=>{
-      if(state.credit>18){
-        alert('You can only choose up to 18 credits in one semester');
+        state.selectedCour = state.selectedCour.filter(
+          (item) => item.courseId != event.target.id
+        );
+        view.pagecrerender(state.credit);
       }
-      else{
-        const response=confirm('You have chosen '+state.credit+' credits for this semester. You cannot change once you submit. Do you want to confirm?')
+    });
 
-      if(response){view.pagerender(state.courses,state.selectedCour,state.credit);}
+    var getbtn = document.querySelector(view.domstr.selectbtn);
+
+    getbtn.addEventListener('click', () => {
+      if (state.credit > 18) {
+        alert('You can only choose up to 18 credits in one semester');
+      } else {
+        const response = confirm(
+          'You have chosen ' +
+            state.credit +
+            ' credits for this semester. You cannot change once you submit. Do you want to confirm?'
+        );
+
+        if (response) {
+          view.pagerender(state.courses, state.selectedCour, state.credit);
+        }
       }
     });
-    
-  });
-    
-    
+  };
+
   const bootstrap = () => {
-    init(),select()
-  }
-  
-  
-    
-  
-    
-return{bootstrap};
-})(Model,View);
+    init(), select();
+  };
+
+  return { bootstrap };
+})(Model, View);
 Controller.bootstrap();
